7e27527f1ef6649f44a3d60b15a3a44223775789
julian-pacilio <julian.pacilio.info@gmail.com>
2024-06-12 13:12:57 -0300

    M2 - Clase 06 - Operadores Logicos

---
+++ modulo-2-javascript/clase-06/notas/operadores-logicos.js
@@ -0,0 +1,23 @@
+// Operadores Lógicos
+// Los operadores lógicos se utilizan para comparar valores entre si, y responden un true si es verdad o un false si es falso.
+
+// And (&&) => Retorna true si ambas condiciones son true
+if (true && true) {
+  // Codigo a ejecutar si ambas condiciones son true
+} else {
+  // Codigo a ejecutar si cualquiera de las condiciones es falsa
+}
+
+// Or (||) => Retorna true si cualquiera de las condiciones es true
+if (true || false) {
+  // Codigo a ejecutar si almenos una condicion son true
+} else {
+  // Codigo a ejecutar si ambas condiciones son false
+}
+
+// Not (!) => Retorna true si la condición no es falsa
+if (!false) {
+  // Codigo a ejecutar si la condicion es true
+} else {
+  // Codigo a ejecutar si la condicion false true
+}
